5673b78f3e470d3eea5dabb8482f49d23c0d3737
Nolan Beard <nolan.beard@eure.jp>
2018-09-26 18:10:29 +0900

    div width is 100% and background picture has been added!

--- resources/views/template-about.blade.php
+++ resources/views/template-about.blade.php
@@ -8,13 +8,28 @@
 @section('content')
   <div id="wrapper">
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    <div id="smallwrapper">
+  <div class="pageheader">
+      @include('partials.page-header')
+    </div>    
+    <div class="smallwrapper">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla magna in hendrerit 
+      scelerisque. Sed interdum elit lectus, at blandit mi ultrices sit amet. Curabitur velit sapien, 
+      eleifend at nisi non, hendrerit vulputate nibh. Suspendisse sollicitudin urna augue, vitae aliquet 
+      velit finibus sed. Maecenas tristique fringilla semper. Nam venenatis justo eget vestibulum feugiat. 
+      Duis quis lectus mollis felis ullamcorper pellentesque ut at dui. Maecenas eget ligula sollicitudin, 
+      fringilla dolor at, commodo justo. Donec viverra semper felis, nec pharetra nunc suscipit at. 
+      Nam ullamcorper, lectus id fringilla convallis, diam tellus egestas risus, vitae rutrum augue
+      orci vitae dolor. Duis tincidunt ultrices nulla nec ultricies. In vitae nisi eu nunc eleifend
+      bibendum at quis magna. Cras ac sem purus. Etiam nec tellus vitae nulla cursus tincidunt id sed nisi.
+      <br><br>
+      Nunc maximus lacus libero, at molestie nibh cursus quis. Donec imperdiet leo ligula, eget faucibus odio
+      iaculis non. Duis in interdum ipsum, vel congue lacus. Fusce at mattis purus. Interdum et malesuada
+      fames ac ante ipsum primis in faucibus. Nulla ut luctus sem. Class aptent taciti sociosqu ad litora 
+      torquent per conubia nostra, per inceptos himenaeos.</p>
       @include('partials.content-page')
-      <h2 id="about">This is meaningful content about our company</h2>
     </div>
   @endwhile
   </div>
 @endsection
 </div>
-
--- resources/views/template-contacts.blade.php
+++ resources/views/template-contacts.blade.php
@@ -6,8 +6,26 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
+    <div class="pageheader">
+      @include('partials.page-header')
+    </div>
+    <div class="smallwrapper">
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla magna in hendrerit 
+      scelerisque. Sed interdum elit lectus, at blandit mi ultrices sit amet. Curabitur velit sapien, 
+      eleifend at nisi non, hendrerit vulputate nibh. Suspendisse sollicitudin urna augue, vitae aliquet 
+      velit finibus sed. Maecenas tristique fringilla semper. Nam venenatis justo eget vestibulum feugiat. 
+      Duis quis lectus mollis felis ullamcorper pellentesque ut at dui. Maecenas eget ligula sollicitudin, 
+      fringilla dolor at, commodo justo. Donec viverra semper felis, nec pharetra nunc suscipit at. 
+      Nam ullamcorper, lectus id fringilla convallis, diam tellus egestas risus, vitae rutrum augue
+      orci vitae dolor. Duis tincidunt ultrices nulla nec ultricies. In vitae nisi eu nunc eleifend
+      bibendum at quis magna. Cras ac sem purus. Etiam nec tellus vitae nulla cursus tincidunt id sed nisi.
+      <br><br>
+      Nunc maximus lacus libero, at molestie nibh cursus quis. Donec imperdiet leo ligula, eget faucibus odio
+      iaculis non. Duis in interdum ipsum, vel congue lacus. Fusce at mattis purus. Interdum et malesuada
+      fames ac ante ipsum primis in faucibus. Nulla ut luctus sem. Class aptent taciti sociosqu ad litora 
+      torquent per conubia nostra, per inceptos himenaeos.</p>
+    </div>
     @include('partials.content-page')
-    <p>This text will not show up because the template has not been applied</p>
   @endwhile
 @endsection
