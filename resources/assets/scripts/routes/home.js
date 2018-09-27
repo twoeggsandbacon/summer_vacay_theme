d67d620618eec41c36aeba3e36684b580fb6e7e6
Nolan Beard <nolan.beard@eure.jp>
2018-09-27 18:05:14 +0900

    Made divs invisible until clicked and added jquery functions

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,6 +1,17 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
+    $(function() {
+      var is_visible = false;
+      $("#home, #about, #contactus").click(function() {
+        var display = "block";
+        if (is_visible) {
+          display = "none";
+        }
+        $(this).find(".description").css("display", display);
+        is_visible = !is_visible;
+      });
+    });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
